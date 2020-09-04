63b3fa21685c98c5d563a6483779ee7346f03e5c
Deepesh <deepesh.kt10636@gmail.com>
2020-09-04 23:04:38 +0530

    added routing path to all components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 
 const routes: Routes = [
   {
+    path : '',
+    pathMatch : 'full',
+    component : HomeComponent
+  },
+  {
     path : 'products',
     pathMatch : 'full',
-    loadChildren : './products.products.module.ts#ProductsModule'
+    loadChildren : './products/products.module#ProductsModule'
   }
 ];
 
