f3e5de7118bf3e38f7a6cabbd19ad17783be4962
Deepesh <deepesh.kt10636@gmail.com>
2020-09-04 19:40:05 +0530

    routed products module in app module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : 'products',
+    pathMatch : 'full',
+    loadChildren : './products.products.module.ts#ProductsModule'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/products/products-routing.module.ts
+++ src/app/products/products-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductsModule } from './products.module';
+import { ProductComponent } from './product/product.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : '',
+    pathMatch : 'full',
+    component : ProductComponent
+  } 
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
