d819b0ee5b2e364f35303499ca703ca623684cae
Deepesh <deepesh.kt10636@gmail.com>
2020-09-20 18:14:16 +0530

    created login function and navigate to home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 
 const routes: Routes = [
@@ -14,6 +15,11 @@ const routes: Routes = [
     pathMatch : 'full',
     component : HomeComponent
   },
+  {
+    path : 'Login',
+    pathMatch : 'full',
+    component : LoginComponent
+  },  
   {
     path : 'products',
     pathMatch : 'full',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PmDemoMaterialModule } from './material-module';
 import { ProductsModule } from './products/products.module';
 import { LoginComponent } from './login/login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -19,6 +20,8 @@ import { LoginComponent } from './login/login.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
     PmDemoMaterialModule,
     ProductsModule
   ],
